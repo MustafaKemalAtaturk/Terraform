ea9060c5b52332a80bb60bb2959ec141750cf8ab
MustafaKemalAtaturk <56719276+MustafaKemalAtaturk@users.noreply.github.com>
2022-01-25 15:36:44 +0000

    a1
    a1

---
+++ s3_bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "devopseduit" {
+  bucket = "devopseduit"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Devosp Homework bucket"
+    Environment = "Dvps"
+  }
+}
+
+resource "aws_s3_bucket_object" "homeworks" {
+    bucket = "devopseduit"
+    acl    = "private"
+    key    = "Homework/"
+}
